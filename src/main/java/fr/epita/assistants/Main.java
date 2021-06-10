eb810ca8408283d44b0405cf1e415e27cf07f4bb
Flavien Darche <11708575+e-n-0@users.noreply.github.com>
2021-06-11 01:39:30 +0200

    Implement Any CLEANUP features (#13)
    * save au cas ou
    
    * Add Cleanup Feature

--- src/main/java/fr/epita/assistants/Main.java
+++ src/main/java/fr/epita/assistants/Main.java
@@ -4,11 +4,8 @@ import java.nio.file.Paths;
 
 import fr.epita.assistants.MyIde.Configuration;
 import fr.epita.assistants.myide.domain.entity.Mandatory;
-import fr.epita.assistants.myide.domain.entity.Node;
 import fr.epita.assistants.myide.domain.entity.Project;
-import fr.epita.assistants.myide.domain.service.NodeService;
 import fr.epita.assistants.myide.domain.service.ProjectService;
-import fr.epita.assistants.utils.Log;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,31 +13,7 @@ public class Main {
         ProjectService projectService = ide.init(new Configuration(Paths.get("."), Paths.get(".")));
         Project project = projectService.load(Paths.get("."));
 
-        // var report =
-        // project.getFeature(Mandatory.Features.Git.PUSH).get().execute(project,
-        // "oui");
-        // System.out.println("report: " + report.isSuccess());
-
-        Node myroot = project.getRootNode();
-
-        Log.log(myroot.getChildren().size() + " - " + myroot.getType().toString());
-
-        NodeService service = projectService.getNodeService();
-        // Log.log(myroot.getPath().toString());
-
-        // Node todel = service.create(myroot, "jesuisundossier", Node.Types.FOLDER);
-        // Log.log(todel.getPath().toString());
-        // Node insiedToDel = service.create(todel, "ouinon", Node.Types.FILE);
-        // Log.log(insiedToDel.getPath().toAbsolutePath().toString());
-
-        /*
-         * var list = myroot.getChildren(); for (Node n : list) { var list2 =
-         * n.getChildren(); Log.log(n.getPath().toString() + " - " +
-         * n.getType().toString() + " - " + list2.size()); for (Node n2 : list2)
-         * Log.log(n2.getPath().toString()); }
-         */
-
-        // service.move(todel, "./test2");
-
+        var report = project.getFeature(Mandatory.Features.Any.CLEANUP).get().execute(project);
+        System.out.println("report: " + report.isSuccess());
     }
 }
--- src/main/java/fr/epita/assistants/myide/ricains/entity/features/project/CleanupFeature.java
+++ src/main/java/fr/epita/assistants/myide/ricains/entity/features/project/CleanupFeature.java
@@ -2,36 +2,60 @@ package fr.epita.assistants.myide.ricains.entity.features.project;
 
 import javax.validation.constraints.NotNull;
 
+import org.assertj.core.util.Files;
+
 import fr.epita.assistants.myide.domain.entity.Feature;
+import fr.epita.assistants.myide.domain.entity.Node;
 import fr.epita.assistants.myide.domain.entity.Project;
 import fr.epita.assistants.myide.domain.entity.Mandatory.Features.Any;
 import fr.epita.assistants.myide.ricains.entity.features.RicainsExecutionReport;
+import fr.epita.assistants.utils.Log;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CleanupFeature implements Feature {
 
+    private static void deleteNode(Node node) {
+        File file = node.getPath().toFile();
+        if (file.exists())
+            file.delete();
+    }
+
+    private static void removeRecusiveFiles(final Node node, final List<String> filenameToDelete) {
+        final String fileName = node.getPath().getFileName().toString();
+        if (filenameToDelete.contains(fileName))
+            deleteNode(node);
+        else
+            for (Node n : node.getChildren())
+                removeRecusiveFiles(n, filenameToDelete);
+    }
+
     @Override
     public @NotNull ExecutionReport execute(Project project, Object... params) {
-        File trashFile = new File(project.getRootNode().getPath().toString() + ".myideignore");
-        try (BufferedReader br = new BufferedReader(new FileReader(trashFile))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                File delete = new File(line);
-                if (delete.exists()) {
-                    delete.delete();
-                }
-                else {
-                    continue;
-                }
+        List<String> filenameToDelete = new ArrayList<>();
+        Path ignoreFilePath = Path.of(project.getRootNode().getPath().toString(), ".myideignore");
+
+        // Get all filename of the .myideignore to delete
+        try (var br = new BufferedReader(new FileReader(ignoreFilePath.toFile()))) {
+            String filename;
+
+            while ((filename = br.readLine()) != null) {
+                Log.log(filename);
+                filenameToDelete.add(filename);
             }
         } catch (IOException e) {
+            Log.err(e);
             return RicainsExecutionReport.create(false);
-            //e.printStackTrace();
         }
+
+        // Search in all files of the project (root node) and delete files
+        removeRecusiveFiles(project.getRootNode(), filenameToDelete);
         return RicainsExecutionReport.create(true);
     }
 
